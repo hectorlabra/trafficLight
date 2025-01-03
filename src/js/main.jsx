db8f03adf436b9d85bda46aa13a3d7e60e96aed6
hectorlabra <121694325+hectorlabra@users.noreply.github.com>
2025-01-03 16:10:11 -0300

    Añadir footer con nombre y enlace de GitHub

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -3,5 +3,23 @@ import ReactDOM from "react-dom/client";
 import TrafficLight from "./components/TrafficLight";
 import "../styles/index.css";
 
+const App = () => (
+  <div>
+    <TrafficLight />
+    <div className="footer">
+      <p>Desarrollado por Héctor Labra</p>
+      <p>
+        <a
+          href="https://github.com/hectorlabra/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+      </p>
+    </div>
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.querySelector("#root"));
-root.render(<TrafficLight />);
+root.render(<App />);
